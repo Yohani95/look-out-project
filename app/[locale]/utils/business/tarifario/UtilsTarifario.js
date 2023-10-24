b880ebc4ae8e67c929f866cb2d2ec43016d4241e
Yohani Espinoza <85854728+Yohani95@users.noreply.github.com>
2023-10-24 19:35:59 -0300

    avances en submit profesionales

--- app/[locale]/utils/business/tarifario/UtilsTarifario.js
+++ app/[locale]/utils/business/tarifario/UtilsTarifario.js
@@ -9,6 +9,74 @@ export const fetchByIdProyecto = async (id) => {
       return [];
     }
   };
+  export const handleFormSubmit =
+  (formData, translations, push, isEditMode = false) =>
+  async (event) => {
+    event.preventDefault();
+    try {
+      const url = isEditMode
+        ? `${proyectoUpdateAsyncApiUrl}/${formData.tcId}`//falta id
+        : `${proyectoCreateAsyncApiUrl}`;
+      const method = isEditMode ? "PUT" : "POST";
+      await NotificationSweet({
+        title: isEditMode
+          ? translations.notification.loading.title
+          : translations.notification.create.title,
+        text: isEditMode
+          ? translations.notification.loading.text
+          : translations.notification.create.text,
+        type: isEditMode
+          ? translations.notification.loading.type
+          : translations.notification.create.type,
+        showLoading: true,
+      });
+      const response = await fetch(url, {
+        method: method,
+        headers: {},
+        body: data,
+      });
+      if (response.ok) {
+        NotificationSweet({
+          title: translations.notification.success.title,
+          text: translations.notification.success.text,
+          type: translations.notification.success.type,
+          push: push,
+          link: "//search",
+        });
+      } else if (response.status === 409) {
+        NotificationSweet({
+          title: translations.notification.warning.title,
+          text: translations.client.clientNameExist,
+          type: translations.notification.warning.type,
+          push: push,
+          link: isEditMode
+            ? `//edit/${formData.cliId}`
+            : "//create",
+        });
+      } else {
+        NotificationSweet({
+          title: translations.notification.warning.title,
+          text: translations.client.clientNameExist,
+          type: translations.notification.warning.type,
+          push: push,
+          link: isEditMode
+            ? `//edit/${formData.cliId}`
+            : "//create",
+        });
+      }
+    } catch (error) {
+      NotificationSweet({
+        title: translations.notification.error.title,
+        text: translations.notification.error.text,
+        type: translations.notification.error.type,
+        push: push,
+        link: "//search",
+      });
+      console.error("Error sending data:", error);
+      // router.push('');
+    }
+  };
+
   export const handleDelete = async (id, trans, fetch) => {
     const confirmed = await ConfirmationDialog(
       trans.notification.deleting.title,
