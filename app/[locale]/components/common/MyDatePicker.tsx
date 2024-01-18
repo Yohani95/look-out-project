efc03bf9c6d0f6bdfe8ab50dd77a7276913b606d
Yohani Espinoza <85854728+Yohani95@users.noreply.github.com>
2024-01-18 10:41:09 -0300

    agregar nueva biblioteca

--- app/[locale]/components/common/MyDatePicker.tsx
+++ app/[locale]/components/common/MyDatePicker.tsx
@@ -1,31 +1,41 @@
 "use client";
-import React, { useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import {es,ptBR,enUS} from 'date-fns/locale'
+import React, { useEffect, useState } from "react";
+import dayjs, { Dayjs } from 'dayjs';
 import { useLocale } from "next-intl";
+import { DatePicker, LocalizationProvider, } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { deDE,esES,ptBR } from '@mui/x-date-pickers/locales';
 const locales = {
-  "es": es,
-  "en": enUS,
-  "br": ptBR
+  "es": esES.components.MuiLocalizationProvider.defaultProps.localeText,
+  "br": ptBR.components.MuiLocalizationProvider.defaultProps.localeText,
 };
-function MyDatePicker({ onChange, selectedDate,isRead=false,title,locale="en",shouldBeRequired = true}) {
+
+function MyDatePicker({ onChange, selectedDate, isRead=false, title, locale="en", shouldBeRequired = true }) {
   const idioma= useLocale();
   const selectedLocale = locales[idioma];
+  const [value, setValue] = useState<Dayjs | null>(dayjs(selectedDate));
+  const handleDateChange = (date: Dayjs) => {
+    setValue(date);
+    onChange(date.format());
+  };
+  console.log(selectedLocale)
   return (
-    <div>
-      <DatePicker
-        className="form-control form-select"
-        selected={selectedDate}
-        onChange={onChange}
-        placeholderText={title || "Date"}
-        dateFormat="dd/MM/yyyy"
-        required={shouldBeRequired}
-        readOnly={isRead}
-        locale={selectedLocale}
-      />
-    </div>
+    <LocalizationProvider  localeText={selectedLocale} dateAdapter={AdapterDayjs}>
+        <DatePicker
+          className="form-control form-select"
+          defaultValue={null}
+          value={value}
+          onChange={handleDateChange}
+          label={title || "Date"}
+          readOnly={isRead}
+          format="DD/MM/YYYY"
+          views={['day', 'month','year', ]}
+          localeText={selectedLocale}
+          
+        />
+    </LocalizationProvider>
   );
 }
+
 
 export default MyDatePicker;
