f6f6e60cda6cc80f1a062fdc0202a7a393fce2e2
Yohani Espinoza <85854728+Yohani95@users.noreply.github.com>
2024-09-30 16:38:32 -0300

    correccion soporte bolsa factura

--- app/[locale]/facture/createBagSupport/[id]/page.tsx
+++ app/[locale]/facture/createBagSupport/[id]/page.tsx
@@ -11,29 +11,45 @@ import HorasUtilizadas from '@/app/api/models/support/HorasUtilizadas';
 import { GetAllEntitiesById } from '@/app/api/actions/soporte/SoporteActions';
 import Soporte from '@/app/api/models/support/Soporte';
 import { GetAllFacturaAdaptacionEntitiesByIdHoras } from '@/app/api/actions/factura/FacturaAdaptacionActions';
+
 async function page({ params }) {
   const locale = await getLocale();
   const t = require(`@/messages/${locale}.json`);
-  //facturasPeriodo
-  let data = new HorasUtilizadas();
+
+  // Obtiene soporte
   const soporte = (await GetAllEntitiesById(params.id)) as Soporte;
+
+  // Verifica si el objeto soporte tiene datos complejos
+  const plainSoporte =
+    typeof soporte === 'object' && soporte.constructor.name !== 'Object'
+      ? JSON.parse(JSON.stringify(soporte))
+      : soporte;
+
+  // Obtiene facturas y facturaAdapcion
   const facturas = (await GetAllEntitiesByIdSoporte(
     params.id
   )) as FacturaPeriodo[];
   const facturaAdapcion = await GetAllFacturaAdaptacionEntitiesByIdHoras(
     params.id
-  ); //
-  data.proyecto = soporte;
+  );
+
+  // Inicializa HorasUtilizadas con los datos de soporte
+  let data = new HorasUtilizadas();
+  data.proyecto = plainSoporte;
   data.id = null;
-  data.fechaPeriodoDesde = soporte.pryFechaInicioEstimada;
-  data.fechaPeriodoHasta = soporte.pryFechaCierreEstimada;
-  data.monto = soporte.pryValor;
+  data.fechaPeriodoDesde = plainSoporte.pryFechaInicioEstimada;
+  data.fechaPeriodoHasta = plainSoporte.pryFechaCierreEstimada;
+  data.monto = plainSoporte.pryValor;
+
+  // Asegúrate de que horasUtilizadas también sea un objeto serializable
+  const plainHorasUtilizadas = JSON.parse(JSON.stringify(data));
+
   return (
     <>
       <BasePages title={t.Nav.facture.requestBilling}>
         <FactureCreate
           t={t}
-          periodo={data}
+          periodo={plainHorasUtilizadas}
           facturas={facturas}
           facturaAdaptacionData={facturaAdapcion}
         />
